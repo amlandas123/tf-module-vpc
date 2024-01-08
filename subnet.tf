e9ada9be5ca7ad33770a2770a992670c4a57f75c
Amlan Das <dasamlan271988@gmail.com>
2024-01-08 06:18:58 +0530

    From amlan

--- subnet.tf
+++ subnet.tf
@@ -1,19 +1,19 @@
-# resource "aws_subnet" "public_subnet" {
-#   count = length(var.PUBLIC_SUBNET_CIDR)
-#   vpc_id     = aws_vpc.main.id
-#   cidr_block = element(var.PUBLIC_SUBNET_CIDR,count.index)
+resource "aws_subnet" "public_subnet" {
+  count = length(var.PUBLIC_SUBNET_CIDR)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = element(var.PUBLIC_SUBNET_CIDR,count.index)
 
-#   tags = {
-#     Name = "roboshop-public"
-#   }
-# }
+  tags = {
+    Name = "roboshop-public"
+  }
+}
 
-# resource "aws_subnet" "private_subnet" {
-#   count = length(var.PRIVATE_SUBNET_CIDR)  
-#   vpc_id     = aws_vpc.main.id
-#   cidr_block = element(var.PRIVATE_SUBNET_CIDR,count.index)
+resource "aws_subnet" "private_subnet" {
+  count = length(var.PRIVATE_SUBNET_CIDR)  
+  vpc_id     = aws_vpc.main.id
+  cidr_block = element(var.PRIVATE_SUBNET_CIDR,count.index)
 
-#   tags = {
-#     Name = "roboshop-private"
-#   }
-# }
+  tags = {
+    Name = "roboshop-private"
+  }
+}
