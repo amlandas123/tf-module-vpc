208541fff019cbd93e6e1cf291887380f792e3ed
Amlan Das <dasamlan271988@gmail.com>
2024-01-11 06:19:41 +0530

    From amlan

---
+++ route_table.tf
@@ -0,0 +1,48 @@
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+#   route {
+#     ipv6_cidr_block        = "::/0"
+#     egress_only_gateway_id = aws_egress_only_internet_gateway.example.id
+#   }
+
+  tags = {
+    Name = "roboshop-${var.ENV}-public_rt"
+  }
+}
+#subnet association of a route table depends on number of subnets(public or private),subnet id and rt_table_id
+resource "aws_route_table_association" "public_subnet_association" {
+  count = length(aws_subnet.public_subnet.*.id)  
+  subnet_id = element(aws_subnet.public_subnet.*.id, count.index)
+  route_table_id = aws_route_table.public_rt.id
+}
+
+
+# resource "aws_route_table" "private_rt" {
+#   vpc_id = aws_vpc.main.id
+
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.igw.id
+#   }
+
+# #   route {
+# #     ipv6_cidr_block        = "::/0"
+# #     egress_only_gateway_id = aws_egress_only_internet_gateway.example.id
+# #   }
+
+#   tags = {
+#     Name = "roboshop-${var.ENV}-private_rt"
+#   }
+# }
+
+# resource "aws_route_table_association" "private_subnet_association" {
+#   count = length(aws_subnet.private_subnet.*.id) 
+#   subnet_id = element(aws_subnet.private_subnet.*.id, count.index)
+#   route_table_id = aws_route_table.private_rt.id
+# }
--- subnet.tf
+++ subnet.tf
@@ -2,9 +2,10 @@ resource "aws_subnet" "public_subnet" {
   count = length(var.PUBLIC_SUBNET_CIDR)
   vpc_id     = aws_vpc.main.id
   cidr_block = element(var.PUBLIC_SUBNET_CIDR,count.index)
+  availability_zone = element(var.AZ,count.index)
 
   tags = {
-    Name = "roboshop-public"
+    Name = "roboshop-${var.ENV}-public-${count.index+1}"
   }
 }
 
@@ -12,8 +13,9 @@ resource "aws_subnet" "private_subnet" {
   count = length(var.PRIVATE_SUBNET_CIDR)  
   vpc_id     = aws_vpc.main.id
   cidr_block = element(var.PRIVATE_SUBNET_CIDR,count.index)
+  availability_zone = element(var.AZ,count.index)
 
   tags = {
-    Name = "roboshop-private"
+    Name = "roboshop-${var.ENV}-private-{count.index+1}"
   }
 }
